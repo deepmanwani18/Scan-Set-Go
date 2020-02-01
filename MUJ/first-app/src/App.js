114dfc0a2aedd696bd89f3ad695cd4de34692fa7
utkarshsingh99 <utkarshsingh369@gmail.com>
2020-02-02 04:23:05 +0530

    Router Switch added

--- MUJ/first-app/src/App.js
+++ MUJ/first-app/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
+import Store from './Store';
+import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Home />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/store">
+            <Store />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
