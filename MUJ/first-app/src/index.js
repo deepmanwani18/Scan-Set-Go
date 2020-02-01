98f9db0bff432e6e50cd526bfb7f3f14c59de49f
deepmanwani18 <deepmanwani18@gmail.com>
2020-02-02 04:10:38 +0530

    react start

--- MUJ/first-app/src/index.js
+++ MUJ/first-app/src/index.js
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
-import React from 'react';
-import ReactDOM from 'react-dom';
-ReactDOM.render(
-<div> 
-    <center>
-    <h1 className="heading">
-        Welcome to SCAN SET GO
-    </h1>
-    </center>
-</div>,
- document.getElementById("root"));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// ReactDOM.render(
+// <div> 
+//     <center>
+//     <h1 className="heading">
+//         Welcome to SCAN SET GO
+//     </h1>
+//     </center>
+// </div>,
+//  document.getElementById("root"));
